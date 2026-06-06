23e59d3143fd6cdf94f734afd19a6d713ebccd59
LorenGrz <lorenzograizzaro55@gmail.com>
2026-06-06 17:28:32 -0300

    Agrego ejercicios de prolog

--- ej4/ej4.pl
+++ ej4/ej4.pl
@@ -1,14 +1,28 @@
-# -- Ejercicio 1: Resolver un problema para una consultora
-# -- ¿Es cierto que alguien programa en Cobol?
-# -- ¿Quién o quienes programan en Cobol?
-# -- ¿Qué lenguajes maneja María?
-# -- ¿Maria y Mario son colegas?
-# -- ¿Quienes son colegas de lenguajes?
-# -- BD:
-# -- programaEn(maria, cobol).
-# -- programaEn(maria, java).
-# -- programaEn(mario, cobol).
-# -- programaEn(mario, python).
-# -- programaEn(jose, cobol).
-# -- programaEn(jorge,java).
-# -- programaEn(jorge,python).
+% # -- Ejercicio 1: Resolver un problema para una consultora
+% # -- BD:
+programaEn(maria, cobol).
+programaEn(maria, java).
+programaEn(mario, cobol).
+programaEn(mario, python).
+programaEn(jose, cobol).
+programaEn(jorge,java).
+programaEn(jorge,python).
+
+% # -- ¿Es cierto que alguien programa en Cobol?
+programaEn(_,cobol).
+% # -- ¿Quién o quienes programan en Cobol?
+programaEn(Programador,cobol).
+% # -- ¿Qué lenguajes maneja María?
+programaEn(maria,Lenguajes).
+% # -- ¿Maria y Mario son colegas?
+sonColegas(Programador1,Programador2) :-
+    programaEn(Programador1, Lenguaje), 
+    programaEn(Programador2, Lenguaje),
+    Programador1 \= Programador2.
+% Tambien se puede consultar directamente
+programaEn(maria,Lenguaje), programaEn(mario, Lenguaje).
+
+% # -- ¿Quienes son colegas de lenguajes?
+sonColegas(Programador1, Programador2)
+o
+?- programaEn(Programador1, Lenguaje), programaEn(Programador2, Lenguaje), Programador1 @< Programador2.
--- ej5/ej5.pl
+++ ej5/ej5.pl
@@ -1,13 +1,24 @@
-# Ejercicio 2: Se pide armar el árbol familiar Real Inglés, usando la relación “esHijoDe”
-# ¿Es cierto que la reina Isabel tiene hijos?	
-# ¿Quién o quienes son hijos de la reina Isabel?
-# ¿Quiénes son los nietos del Carlos III?
-# ¿Es cierto que Archie es primo del príncipe Jorge?
-# ¿Quienes son primos?
-# BD:
-# esHijoDe(carlosIII, isabelII).
-# esHijoDe(ana, isabelII).
-# esHijoDe(andrew, isabelII).
-# esHijoDe(edward, isabelII).
-# esHijoDe(henry, carlosII).
-# ……….
+% # Ejercicio 2: Se pide armar el árbol familiar Real Inglés, usando la relación “esHijoDe”
+% # ¿Quienes son primos?
+% # BD:
+esHijoDe(carlosIII, isabelII).
+esHijoDe(ana, isabelII).
+esHijoDe(andrew, isabelII).
+esHijoDe(edward, isabelII).
+esHijoDe(henry, carlosIII).
+% # ¿Es cierto que la reina Isabel tiene hijos?	
+esHijoDe(_,isabelII).
+% # ¿Quién o quienes son hijos de la reina Isabel?
+esHijoDe(Hijos,isabelII).
+% # ¿Quiénes son los nietos del Carlos III?
+esNieto(Nieto,Abuelo) :-
+    esHijoDe(Nieto,Padre),
+    esHijoDe(Padre,Abuelo),
+    Nieto \= Abuelo.
+% # ¿Es cierto que Archie es primo del príncipe Jorge?
+esPrimo(Primo1,Primo2):-
+    esHijoDe(Primo1, Padre1),
+    esHijoDe(Primo2, Padre2),
+    esHijoDe(Padre1, Abuelo),
+    esHijoDe(Padre2, Abuelo),
+    Primo1 \= Primo2, Padre1\=Padre2.
