81db9bad7641c7f5b133879aba6c42bf635383d2
LorenGrz <lorenzograizzaro55@gmail.com>
2026-06-06 21:03:17 -0300

    Termine, final commit

--- cartas/cartas.pl
+++ cartas/cartas.pl
@@ -1,10 +1,10 @@
-# Generador de cartas:
-# cartaDeTruco(carta(Numero,Palo)):- palo(Palo), numeroValido(Numero).
-# palo(basto).
-# palo(oro).
-# palo(espada).
-# palo(copa).
-# numeroValido(N):- between(1,12,N), not between(8,9,N).
-# Nuevo predicado mata:
-# mata(Mayor, Menor):-cartaDeTruco(Mayor), cartaDeTrucoMenor, valeMas(Mayor,Menor).
-# mata(Mayor,Menor):-cartaDeTruco(Mayor), cartaDeTruco(Otra), valeMas(Mayor,Otra), mata(Otra,Menor).
+% # Generador de cartas:
+cartaDeTruco(carta(Numero,Palo)):- palo(Palo), numeroValido(Numero).
+palo(basto).
+palo(oro).
+palo(espada).
+palo(copa).
+numeroValido(N):- between(1,12,N), not between(8,9,N).
+% Nuevo predicado mata:
+mata(Mayor, Menor):-cartaDeTruco(Mayor), cartaDeTrucoMenor, valeMas(Mayor,Menor).
+mata(Mayor,Menor):-cartaDeTruco(Mayor), cartaDeTruco(Otra), valeMas(Mayor,Otra), mata(Otra,Menor).
