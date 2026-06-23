1101382040e28ac32aeaa1a762f6368925b02ef9
LorenGrz <lorenzograizzaro55@gmail.com>
2026-06-23 15:59:52 -0300

    Agrego ejercicios haskell para el ultimo parcial

--- ej4/ej4.hs
+++ ej4/ej4.hs
@@ -1,9 +1,4 @@
 -- Ejercicio 1: Resolver un problema para una consultora
--- ¿Es cierto que alguien programa en Cobol?
--- ¿Quién o quienes programan en Cobol?
--- ¿Qué lenguajes maneja María?
--- ¿Maria y Mario son colegas?
--- ¿Quienes son colegas de lenguajes?
 -- BD:
 -- programaEn(maria, cobol).
 -- programaEn(maria, java).
@@ -12,3 +7,28 @@
 -- programaEn(jose, cobol).
 -- programaEn(jorge,java).
 -- programaEn(jorge,python).
+type Nombre = String
+type Lenguaje = String
+data Persona = Persona
+{
+    nombre :: Nombre,
+    lenguajes :: [Lenguaje]
+}   deriving (Show, Eq)
+
+-- ¿Es cierto que alguien programa en Cobol?
+programaEn :: Lenguaje -> [Persona] -> Bool
+programaEn lenguaje personas = elem lenguaje (concatMap lenguajes personas)
+-- Tambien puede ser
+programaEn lenguaje personas = any (\persona -> elem lenguaje (lenguajes persona)) personas
+
+-- ¿Qué lenguajes maneja María?
+queLenguajesManeja :: Persona -> [Lenguaje]
+queLenguajesManeja persona = lenguajes persona
+
+-- ¿Quién o quienes programan en Cobol?
+quienProgramaEn :: Lenguaje -> [Persona] -> [Nombre]
+quienProgramaEn lenguaje personas = map nombre (filter (\persona -> elem lenguaje (lenguajes persona)) personas)
+
+-- ¿Maria y Mario son colegas?
+sonColegas :: Persona -> Persona -> Bool
+sonColegas unaPersona otraPersona = any (\lenguaje -> elem lenguaje (lenguajes unaPersona)) (lenguajes otraPersona)
--- ej5/ej5.hs
+++ ej5/ej5.hs
@@ -1,7 +1,4 @@
 -- Ejercicio 2: Se pide armar el árbol familiar Real Inglés, usando la relación “esHijoDe”
--- ¿Es cierto que la reina Isabel tiene hijos?	
--- ¿Quién o quienes son hijos de la reina Isabel?
--- ¿Quiénes son los nietos del Carlos III?
 -- ¿Es cierto que Archie es primo del príncipe Jorge?
 -- ¿Quienes son primos?
 -- BD:
@@ -11,3 +8,25 @@
 -- esHijoDe(edward, isabelII).
 -- esHijoDe(henry, carlosII).
 -- ……….
+type Nombre = String
+
+data Persona = Persona 
+  { nombre :: Nombre
+  , padres :: [Nombre] -- Lista con los nombres de sus padres
+  } deriving (Show, Eq)
+
+esHijoDe :: Persona -> Persona -> Bool
+esHijoDe padre hijo = elem (nombre padre) (padres hijo)
+
+-- ¿Es cierto que la reina Isabel tiene hijos?	
+tieneHijos :: Persona -> [Persona] -> Bool
+tieneHijos unaPersona personas = any (esHijoDe unaPersona) personas
+-- Se puede usar lambda (\p -> esHijoDe persona p) pero se puede usar de la otra forma mas reducida
+
+-- ¿Quién o quienes son hijos de la reina Isabel?
+quienesSonHijos :: Persona -> [Persona] -> [Persona]
+quienesSonHijos unaPersona personas = filter (esHijoDe unaPersona) personas
+
+-- ¿Quiénes son los nietos del Carlos III?
+quienesSonNietos :: Persona -> [Persona] -> [Persona]
+quienesSonNietos abuelo personas = concatMap (\padre -> quienesSonHijos padre personas) (quienesSonHijos abuelo personas)
